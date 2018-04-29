e528dec4c9fee8971250b6859852bb0f16e4de92
Ernest Gottschalk <ayrnee@gmail.com>
2018-04-29 15:54:22 -0400

    login added

--- app.py
+++ app.py
@@ -7,50 +7,38 @@ app.config['MYSQL_PASSWORD'] = '1uXgh92Fwi'
 app.config['MYSQL_DB'] = 'sql9233071'
 mysql = MySQL(app)
 
-tables = {}
-def findtables(substr):
-    ans = {}
-    for table in tables:
-        if table.find(substr) > -1:
-            ans[table] = tables[table]
-    return ans
+# valid_user(usr,psw):
+#     cur = mysql.connection.cursor()
 
-def find_attr(item):
-    ans = {}
-    for table in tables:
-        if item in tables[table]:
-            ans[table] = tables[table]
-    return ans
 
-def get_attr(name):
-    return tables[field]
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        username =  request.form['usr']
+        password = request.form['psw']
+        print username
+        print password
+        str = '''SELECT userid FROM USERS where uname='''+"'" +username+"'"
+        print str
+        cur.execute(str)
+        ans = cur.fetchall()
+        print ans
 
-@app.route('/<item>', methods=['GET', 'POST'])
-def attr_router(item):
-    target_tables = find_attr(item)
-    return render_template('data.html', target_tables=target_tables)
+        return render_template('login.html')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT userid FROM USERS''')
+    uname = cur.fetchall()
+    print uname
+    return render_template('register.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT table_name FROM information_schema.tables where table_type="base table"''')
-    t_names = cur.fetchall()
-    i = 0
-    for name in t_names:
-            str = "DESCRIBE" +' '+ name[0]
-            cur.execute(str)
-            t_vals = cur.fetchall()
-            vals = []
-
-            for j in range(len(t_vals)):
-                vals.append(t_vals[j][0])
-                ++j
-            tables[name[0]] = vals
-            ++i
-
-    if request.method == 'POST':
-        target_tables = findtables(request.form['target'])
-        return render_template('data.html', target_tables=target_tables)
 
     return render_template('index.html')
 if __name__ == "__main__":
